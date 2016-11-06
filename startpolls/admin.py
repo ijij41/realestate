f344ae9b2df989df19c4d6cb7704a65505a90700
Junghoon Lee <ijij41@gmail.com>
2016-11-06 13:21:01 +0900

    init

---
+++ startpolls/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from startpolls.models import Question
+
+admin.site.register(Question)
---
+++ startpolls/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+from startpolls.views import HomePageView
+
+urlpatterns = [
+    url(r'^$', HomePageView.as_view(),name="index"),
+]
