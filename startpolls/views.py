2d35de7bc01c14273d0e81e6f57c99346df920da
Junghoon Lee <ijij41@gmail.com>
2016-11-27 18:39:09 +0900

    delte startpoll example

--- startpolls/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.views.generic import TemplateView
-
-
-class HomePageView(TemplateView):
-    template_name = "startpolls/home.html"
-
